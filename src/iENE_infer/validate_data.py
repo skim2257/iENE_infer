a73e686755dca96b106779512656b8f55015d296
skim2257 <skim2257>
2025-06-18 11:14:10 -0400

    feat: validation step stdout

--- src/iENE_infer/validate_data.py
+++ src/iENE_infer/validate_data.py
@@ -6,6 +6,9 @@ parser = ArgumentParser(description="Validate NRRD images")
 parser.add_argument("--root_directory", type=str, required=True, help="Path to the root directory containing NRRD images")
 args = parser.parse_args()
 
+print("Validating NRRD images in:", args.root_directory)
+print(len(os.listdir(args.root_directory)), "files found.")
+
 for path in glob.glob(os.path.join(args.root_directory, "*.nrrd")):
     image = sitk.ReadImage(path)
     # Perform validation on the image
